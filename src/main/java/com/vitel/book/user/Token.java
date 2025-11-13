ada7ec36a8ae40ea86a4e2d368ae74ddba1853cf
jose villegas <josevillegas13600@gmail.com>
2025-11-13 16:40:05 -0400

    creacion de la clase token y repositorio token

---
+++ src/main/java/com/vitel/book/user/Token.java
@@ -0,0 +1,28 @@
+package com.vitel.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validateAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
